a09f9b8bd8242d96f6376df991bb9dc2d4d61f41
Philip King <ilovethechiefs@gmail.com>
2015-06-20 15:11:08 -0500

    Removing cases has odd glitch.  Multiple consecutive removals changes height multiplicatively.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var singleCaseHTML = '<div class="singleCase"><p>Patient info</p><ul>\
-                    <li>Button 1</li><li>Button 2</li><li>Button 3</li>\
-                    <li>Button 4</li><li>Button 5</li></ul></div><hr>';
+                    <li>Scheduling</li><li>IDX</li><li>Precert</li>\
+                    <li>Confirmation</li><li>Cancel/Reschedule</li></ul></div>';
 
 var main = function() {
   $("#addCaseButton").click(addCase);
@@ -9,7 +9,42 @@ var main = function() {
 $(document).ready(main);
 
 function addCase() {
-  $("#casesDiv").css("border", "solid");
   $("#casesDiv").append(singleCaseHTML);
-  $("#casesDiv").height("+=84px");
+  $("#casesDiv").height("+=70px");
+  if ($("#casesDiv").children().length == 1) {
+    alert("First case");
+    $("#casesDiv").css("border", "solid");
+    $(".singleCase ul li:nth-child(1)").on("click", scheduleButton);
+    $(".singleCase ul li:nth-child(2)").on("click", idxButton);
+    $(".singleCase ul li:nth-child(3)").on("click", precertButton);
+    $(".singleCase ul li:nth-child(4)").on("click", confirmationButton);
+    $(".singleCase ul li:nth-child(5)").on("click", cancelButton);
+  }
+
+
+}
+
+function scheduleButton() {
+  alert("scheduleButton");
+}
+
+function idxButton() {
+  alert("idxButton");
+}
+
+function precertButton() {
+  alert("precertButton");
+}
+
+function confirmationButton() {
+  alert("confirmationButton");
+}
+
+function cancelButton() {
+  $(this).parent().parent().remove();
+  if ($("#casesDiv").children().length == 0) {
+    alert("Removed only case");
+    $("#casesDiv").css("border", "none");
+  }
+  $("#casesDiv").height('-=70px');
 }
