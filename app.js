ee19f94fddfbbf87219366b8aaa00b07cae3abbc
Philip King <ilovethechiefs@gmail.com>
2015-06-21 18:23:11 -0500

    IDX and Precert forms finished.  Buttons all working.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var singleCaseHTML = '<div class="singleCase"><p>Patient info</p><ul>\
                     <li><p>Scheduling</p></li><li><p>IDX</p></li><li><p>Precert</p></li>\
                     <li><p>Confirmation</p></li><li><p>Cancel/Reschedule</p></li></ul></div>';
 
-var scheduleDataEntryHTML = '<div id="darkenPage"></div><div id="dataEntry"><div id="dataEntryFieldsDiv">\
+var scheduleFormHTML = '<div id="darkenPage"></div><div id="scheduleForm"><div id="scheduleFormFieldsDiv">\
                             <table>\
                             <tr>\
                             <td>Today\'s Date:</td><td><input type="text" name="todaysDateInput"></td>\
@@ -56,13 +56,84 @@ var scheduleDataEntryHTML = '<div id="darkenPage"></div><div id="dataEntry"><div
                             </tr>\
                             </table>\
                             </div>\
-                            <div id="dataEntryButtonDiv"><button id="saveButton" type="button">\
-                            Save</button><button id="closeButton" type="button">\
+                            <div id="scheduleFormButtonDiv"><button id="scheduleSaveButton" type="button">\
+                            Save</button><button id="scheduleCloseButton" type="button">\
                             Close without saving</button></div>\
                             </div>';
 
+var idxFormHTML = '<div id="darkenPage"></div><div id="idxForm"><div id="idxFormFieldsDiv">\
+                  <table>\
+                  <th colspan=2>Has this case been entered into IDX?</th>\
+                  <tr>\
+                  <td>Yes</td><td>No</td>\
+                  </tr>\
+                  </table>\
+                  </div>\
+                  <div id="idxFormButtonDiv"><button id="idxSaveButton" type="button">\
+                  Save</button><button id="idxCloseButton" type="button">\
+                  Close without saving</button></div>\
+                  </div>';
 
-
+var precertFormHTML = '<div id="darkenPage"></div><div id="precertForm"><div id="precertFormFieldsDiv">\
+                      <table>\
+                      <tr>\
+                      <td>Patient Name:</td><td><input type="text" name="patientNameInput"></td>\
+                      </tr>\
+                      <tr>\
+                      <td>Date of Birth:</td><td><input type="text" name="dateOfBirthInput"></td>\
+                      </tr>\
+                      <tr>\
+                      <td>Procedure:</td><td><input type="text" name="procedureNumber'+procedureNumber+'"></td>\
+                      <td>Side:</td><td><input type="text" name="procedureSide'+procedureNumber+'"></td>\
+                      <td><button id="addProcedureButton'+procedureNumber+'" type="button">Add procedure</button>\
+                      </tr>\
+                      </table>\
+                      <hr>\
+                      <table>\
+                      <tr>\
+                      <td>Insurance:</td><td><input type="text" name="insurance'+insuranceNumber+'"></td>\
+                      <td>Policy Number:</td><td><input type="text" name="policyNumber'+insuranceNumber+'"></td>\
+                      <td>Precert Phone:</td><td><input type="text" name="phoneNumber'+insuranceNumber+'"></td>\
+                      </tr>\
+                      </table>\
+                      <table id="precertInsuranceTable'+insuranceNumber+'">\
+                      <tr>\
+                      <td>Precert notes:</td>\
+                      </tr>\
+                      <tr>\
+                      <td><textarea id="precertNotes'+insuranceNumber+'"></textarea></td>\
+                      </tr>\
+                      <tr>\
+                      <td><button id="precertAddInsuranceButton'+insuranceNumber+'" type="button">Add insurance</button></td>\
+                      </tr>\
+                      </table>\
+                      <hr>\
+                      <table id="precertNotesTable">\
+                      <tr>\
+                      <td>Notes for Precertification Coordinator:</td>\
+                      <td>Notes for Reservation:</td>\
+                      </tr>\
+                      <tr>\
+                      <td><textarea id="notesForPrecert"></textarea></td>\
+                      <td><textarea id="notesForReservation"></textarea></td>\
+                      </tr>\
+                      </table>\
+                      <hr>\
+                      <table id="reservationTasking">\
+                      <tr>\
+                      <td>OR Scheduling:</td><td><input type="text" name="taskToORScheduling"></td>\
+                      <td>Pre-Registration:</td><td><input type="text" name="taskToPreReg"></td>\
+                      </tr>\
+                      <tr>\
+                      <td>Precert entered in IDX:</td><td><input type="text" name="precertInIDX"></td>\
+                      <td>Precert written in chart:</td><td><input type="text" name="precertInChart"></td>\
+                      </tr>\
+                      </table>\
+                      </div>\
+                      <div id="precertFormButtonDiv"><button id="precertSaveButton" type="button">\
+                      Save</button><button id="precertCloseButton" type="button">\
+                      Close without saving</button></div>\
+                      </div>';
 
 var main = function() {
   $("#addCaseButton").click(addCase);
@@ -85,21 +156,36 @@ function addCase() {
 }
 
 function scheduleButton() {
-  $("html").append(scheduleDataEntryHTML);
-  $("#dataEntry").height(.75*screen.height)
+  $("html").append(scheduleFormHTML);
+  $("#scheduleForm").height(.75*screen.height)
   $("#addPhoneButton1").on("click", addPhoneNumber);
   $("#addProcedureButton1").on("click", addProcedure);
   $("#addInsuranceButton1").on("click", addInsurance);
-  $("#saveButton").on("click", closeScheduleForm);
-  $("#closeButton").on("click", closeScheduleForm);
+  $("#scheduleSaveButton").on("click", closeScheduleForm);
+  $("#scheduleCloseButton").on("click", closeScheduleForm);
+  $('body').css("overflow", "hidden");
 }
 
 function idxButton() {
-  alert("idxButton");
+  $("html").append(idxFormHTML);
+  $("#idxForm").height(.35*screen.height)
+  $("#idxFormFieldsDiv table td").on("click", function() {
+    $("#idxFormFieldsDiv table td").css("background-color", "white")
+    $(this).css("background-color", "#cccccc");
+  });
+  $("#idxSaveButton").on("click", closeIDXForm);
+  $("#idxCloseButton").on("click", closeIDXForm);
+  $('body').css("overflow", "hidden");
 }
 
 function precertButton() {
-  alert("precertButton");
+  $("html").append(precertFormHTML);
+  $("#precertForm").height(.75*screen.height)
+  $("#precertAddInsuranceButton1").on("click", precertAddInsurance);
+  $("#precertNotes1").width(.70*screen.width).height(.2*screen.height);
+  $('body').css("overflow", "hidden");
+  $("#precertSaveButton").on("click", closePrecertForm);
+  $("#precertCloseButton").on("click", closePrecertForm);
 }
 
 function confirmationButton() {
@@ -131,7 +217,7 @@ function addPhoneNumber() {
 
 function addProcedure() {
   procedureNumber++;
-  var newProcedureHTML = '</tr><tr>\
+  var newProcedureHTML = '</tr></table><table><tr>\
                           <td>Procedure:</td><td><input type="text" name="procedureNumber'+procedureNumber+'"></td>\
                           <td>Side:</td><td><input type="text" name="procedureSide'+procedureNumber+'"></td>\
                           <td><button id="addProcedureButton'+procedureNumber+'" type="button">Add procedure</button>';
@@ -154,5 +240,45 @@ function addInsurance() {
 
 function closeScheduleForm() {
   $('#darkenPage').remove();
-  $('#dataEntry').remove();
+  $('#scheduleForm').remove();
+  $('body').css("overflow", "auto");
+}
+
+function closeIDXForm() {
+  $('#darkenPage').remove();
+  $('#idxForm').remove();
+  $('body').css("overflow", "auto");
+}
+
+function closePrecertForm() {
+  $('#darkenPage').remove();
+  $('#precertForm').remove();
+  $('body').css("overflow", "auto");
+}
+
+function precertAddInsurance() {
+  insuranceNumber++;
+  var newInsuranceHTML = '<hr>\
+                          <table>\
+                          <tr>\
+                          <td>Insurance:</td><td><input type="text" name="insurance'+insuranceNumber+'"></td>\
+                          <td>Policy Number:</td><td><input type="text" name="policyNumber'+insuranceNumber+'"></td>\
+                          <td>Precert Phone:</td><td><input type="text" name="phoneNumber'+insuranceNumber+'"></td>\
+                          </tr>\
+                          </table>\
+                          <table id="precertInsuranceTable'+insuranceNumber+'">\
+                          <tr>\
+                          <td>Precert notes:</td>\
+                          </tr>\
+                          <tr>\
+                          <td><textarea id="precertNotes'+insuranceNumber+'"></textarea></td>\
+                          </tr>\
+                          <tr><td><button id="precertAddInsuranceButton'+insuranceNumber+'" type="button">Add insurance</button></td>\
+                          </tr>\
+                          </table>';
+
+  $(newInsuranceHTML).insertAfter($("#precertInsuranceTable"+(insuranceNumber-1)));
+  $("#precertAddInsuranceButton"+insuranceNumber).on("click", precertAddInsurance);
+  $("#precertAddInsuranceButton"+(insuranceNumber-1)).remove();
+  $("#precertNotes"+insuranceNumber).width(.70*screen.width).height(.2*screen.height);
 }
